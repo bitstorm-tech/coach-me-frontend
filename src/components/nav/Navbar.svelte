26b466242ed1f8add95d8a940efc537d0abc9fc6
Josef Bauer <josef.bauer.1st@gmail.com>
2021-08-20 14:46:17 +0200

    Ongoing Navbar port

--- src/components/nav/Navbar.svelte
+++ src/components/nav/Navbar.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
   import type NavbarItemType from './NavbarItem.type';
   import NavbarItem from './NavbarItem.svelte';
+  import CloseSymbol from '../ui/CloseSymbol.svelte';
+  import HamburgerSymbol from '../ui/HamburgerSymbol.svelte';
+  import { slide } from 'svelte/transition';
 
   export let navbarItems: NavbarItemType[] = [];
+  let showMobileMenu = false;
 
-  function toggleMobileMenu() {}
-  function closeMobileMenu() {}
+  function toggleMobileMenu() {
+    showMobileMenu = !showMobileMenu;
+  }
+  function closeMobileMenu() {
+    showMobileMenu = false;
+  }
 </script>
 
 <nav class="flex flex-col sm:flex-row bg-darkest justify-between sm:h-14 p-2 top-0">
   <div class="flex flex-row justify-between items-center">
-    <div class="cursor-pointer flex flex-row space-x-2 font-lg" onClick={closeMobileMenu}>
+    <div class="cursor-pointer flex flex-row space-x-2 font-lg" on:click={closeMobileMenu}>
       <img src="/images/coach-me-logo.png" alt="Logo" height="28px" width="28px" />
       <NavbarItem href="/">c04ch.ME</NavbarItem>
     </div>
     <div class="sm:hidden" on:click={toggleMobileMenu}>
-      <!-- {mobileMenuVisible ? <CloseSymbol /> : <HamburgerSymbol />} -->
+      {#if showMobileMenu}
+        <CloseSymbol />
+      {:else}
+        <HamburgerSymbol />
+      {/if}
     </div>
   </div>
-  <!-- {renderDesktopMenu()} -->
+  <!-- Desktop Menu -->
   <div class="hidden sm:flex flex-row items-center w-min">
     {#each navbarItems as navbarItem}
       {#if navbarItem.visible}
@@ -31,6 +43,22 @@
     {/each}
     <!-- {renderAuthButtons()} -->
   </div>
-  <!-- {renderMobileMenu()} -->
+  <!-- Mobile Menu -->
+  {#if showMobileMenu}
+    <div class="flex flex-col items-center w-full mt-4 sm:hidden" transition:slide>
+      {#each navbarItems as navbarItem}
+        {#if navbarItem.visible}
+          <div class="mt-4" on:click={closeMobileMenu}>
+            <NavbarItem href={navbarItem.href}>
+              {navbarItem.text}
+            </NavbarItem>
+          </div>
+        {/if}
+      {/each}
+      <div class="mt-10">
+        <!-- {renderAuthButtons()} -->
+      </div>
+    </div>
+  {/if}
 </nav>
 <!-- {renderLoginModal()} -->
